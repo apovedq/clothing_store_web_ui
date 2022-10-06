e10e2895966f946de9e63c1d6b58937b42f5617d
apovedq <povedandres11@gmail.com>
2022-10-06 09:34:23 -0500

    Added details page and fixed page routing

---
+++ details/index.js
@@ -0,0 +1,27 @@
+let nameQueryParam = new URLSearchParams(window.location.search).get('name');
+let product_detail;
+
+searchProduct();
+renderProduct();
+
+function searchProduct() { 
+    product_detail = products.find((item) => nameQueryParam === item.name);
+    console.log(product_detail);
+}
+
+function renderProduct() {
+    const title = document.getElementById('detail-product-title');
+    const tag = document.getElementById('detail-product-tag');
+    const price = document.getElementById('detail-product-price');
+    const description = document.getElementById('detail-product-description');
+    const img = document.getElementById('detail-product-img');
+
+
+    title.textContent = product_detail.name;
+    tag.textContent = product_detail.tag;
+    price.textContent = product_detail.price;
+    description.textContent = product_detail.description;
+
+    img.setAttribute('src', product_detail.imageURL);
+
+}
---
+++ products/products_list.js
@@ -0,0 +1,75 @@
+const products = [
+    {
+        imageURL: "https://cdn.shopify.com/s/files/1/0019/1863/1981/products/SACOMANZANILLAFRENTE_700x.jpg?v=1658781549",
+        tag: "hoodie",
+        name: "Manzanilla Art",
+        description: "Linea Botánica, Coleccion de Hierbas, Hoodie 100% Algodon, ¿Qué esperas?",
+        price: "$350,000",
+    },
+
+    {
+        imageURL: "https://cdn.shopify.com/s/files/1/0019/1863/1981/products/camisetalavandacruda_700x.jpg?v=1658789670",
+        tag: "t-shirt",
+        name: "Lavanda crudo",
+        description: "Linea Botánica, Coleccion de Hierbas, Hoodie 100% Algodon, ¿Qué esperas?",
+        price: "$110,000",
+    },
+
+    {
+        imageURL: "https://cdn.shopify.com/s/files/1/0019/1863/1981/products/bermudamanzanillafrentenegra_700x.jpg?v=1658782121",
+        tag: "shorts",
+        name: "Manzanilla negro",
+        description: "Linea Botánica, Coleccion de Hierbas, Hoodie 100% Algodon, ¿Qué esperas?",
+        price: "$100,000",
+    },
+
+    {
+        imageURL: "https://cdn.shopify.com/s/files/1/0019/1863/1981/products/hoodiehierbasatras_700x.jpg?v=1658772343",
+        tag: "hoodie",
+        name: "Bomber Jacket",
+        description: "De estilo bomber, esta chaqueta se integra a TRUE, Sientete TRUE, VIVE TRUE",
+        price: "$330.000",
+    },
+
+    {
+        imageURL: "https://cdn.shopify.com/s/files/1/0019/1863/1981/products/Pantalonetagatofrente_ec08b78e-2e6a-4758-9ffb-813d209c6f36_700x.jpg?v=1662916802",
+        tag: "shorts",
+        name: "Board Shorts - Waves",
+        description: "Está hecho en un tejido fluidopensado para las exigencias de la playa, el sol o la piscina.",
+        price: "$130.000",
+    },
+
+    {
+        imageURL: "https://cdn.shopify.com/s/files/1/0019/1863/1981/products/camisetadelfinbco_700x.jpg?v=1662916423",
+        tag: "t-shirt",
+        name: "Love Story",
+        description: "Llega con una silueta de corte clásico con bordado tipográfico frontal",
+        price: "$140.000",
+    },
+
+    {
+        imageURL: "https://cdn.shopify.com/s/files/1/0019/1863/1981/products/bermudahierbasfrente_700x.jpg?v=1658786789",
+        tag: "shorts",
+        name: "Cargo nude",
+        description: "Está hecho en un tejido fluidopensado para las exigencias de la playa, el sol o la piscina.",
+        price: "$130.000",
+    },
+
+    {
+        imageURL: "https://cdn.shopify.com/s/files/1/0019/1863/1981/products/camisetastonersbeeratras_700x.jpg?v=1662918693",
+        tag: "t-shirt",
+        name: "La Vie en Rose",
+        description: "Posee bordados tipográficos en punto corazón y espalda. La mejor amiga",
+        price: "$115.000",
+    },
+
+    {
+        imageURL: "https://cdn.shopify.com/s/files/1/0019/1863/1981/products/microdoserosado_700x.jpg?v=1655392690",
+        tag: "hoodie",
+        name: "Microdose",
+        description: "De estilo bomber, esta chaqueta se integra a TRUE, Sientete TRUE, VIVE TRUE",
+        price: "$330.000",
+    },
+]
+
+//export default products;
